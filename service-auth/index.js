c636d701025c142ab97cb90f70fc2c8233ec940d
Justine Quatresols <justine.quatresols@viacesi.fr>
2023-06-07 15:22:16 +0200

    wip connexion mysql

--- service-auth/index.js
+++ service-auth/index.js
@@ -1,22 +1,38 @@
-const express = require('express');
-const os = require('os');
-const mysql = require("mysql");
+const express = require("express");
+const os = require("os");
+const mysql = require("mysql2");
 
+setTimeout(() => {
+  const con = mysql.createConnection({
+    host: "localhost",
+    user: "user",
+    password: "password",
+    port: "3306",
+    database: "cesi",
+  });
+
+  con.connect((err) => {
+    if (err) {
+      console.log("not connected");
+      console.log(err);
+    } else {
+      console.log("connected");
+    }
+  });
+}, 20000);
 
 const app = express();
 const port = 3000;
 
-var cpuUsage = null
+var cpuUsage = null;
 
 app.get("/login", (req, res) => {
-  res.send('Login ✅');
+  res.send("Login ✅");
 });
 
-app.get('/getCPU', (req, res) => {
+app.get("/getCPU", (req, res) => {
   res.send({ cpu: cpuUsage });
 });
-
-
 
 app.listen(port, () => {
   console.log(`Authentication service is running on http://servicex:${port}`);
