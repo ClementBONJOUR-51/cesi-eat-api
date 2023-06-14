088572615ad7e553873150d107bc24184f721378
Justine Quatresols <justine.quatresols@viacesi.fr>
2023-06-14 14:07:23 +0200

    JWT push

--- service-auth/tokenChecker.js
+++ service-auth/tokenChecker.js
@@ -1,24 +1,63 @@
-const jwt = require('jsonwebtoken')
-const config = require('./config')
+const jwt = require("jsonwebtoken");
+const config = require("./config");
+const { all } = require("axios");
 
-module.exports = (req,res,next) => {
-  const token = req.body.token || req.query.token || req.headers['x-access-token']
-  // decode token
-  if (token) {
-    // verifies secret and checks exp
-    jwt.verify(token, config.secret, function(err, decoded) {
+module.exports = (allowedRoles) => {
+  return (req, res, next) => {
+    const token =
+      req.body.token || req.query.token || req.headers["x-access-token"];
+
+    // Decode token
+    if (token) {
+      // Verify secret and check expiration
+      jwt.verify(token, config.secret, function (err, decoded) {
         if (err) {
-            return res.status(401).json({"error": true, "message": 'Unauthorized access.' });
+          return res
+            .status(401)
+            .json({ error: true, message: "Unauthorized access." });
         }
-      req.decoded = decoded;
-      next();
-    });
-  } else {
-    // if there is no token
-    // return an error
-    return res.status(403).send({
-        "error": true,
-        "message": 'No token provided.'
-    });
-  }
-}
+
+        req.decoded = decoded;
+
+        console.log(allowedRoles);
+        var nextok = false;
+        if (!allowedRoles.length) {
+          nextok = true;
+          next();
+        }
+        //foreach allowedroles and next() if decoded[i] === 1
+        allowedRoles.forEach((role) => {
+          if (decoded[role] === 1) {
+            nextok = true;
+            next();
+          }
+        });
+        if (!nextok) {
+          return res
+            .status(403)
+          .json({ error: true, message: "Unauthorized access."});
+        
+        }
+      
+
+
+        // if (userCustomer === 1) {
+        //   // L'utilisateur a le rôle d'administrateur, continuez le traitement
+        //   next();
+        // } else {
+        //   // L'utilisateur n'a pas les autorisations nécessaires
+        //   return res
+        //     .status(403)
+        //     .json({ error: true, message: "Unauthorized access." });
+        // }
+      });
+    } else {
+      // If there is no token
+      // Return an error
+      return res.status(403).send({
+        error: true,
+        message: "No token provided.",
+      });
+    }
+  };
+};
